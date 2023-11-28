741d571ffe51e874b6f9d3c4b9dac2a64bfaded0
vinicius.machado <supersoftdev01@outlook.com>
2023-11-28 15:58:24 -0300

    commit inicial

---
+++ lib/one_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:navegacao_paginas/two_page.dart';
+
+class OnePage extends StatelessWidget {
+  const OnePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => const TwoPage(args: 'teste')));
+          },
+          child: Text('Ir para Segunda Page'),
+        ),
+      ),
+    );
+  }
+}
