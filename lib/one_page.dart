05f08dc6577cfc5cdf000314c760069b039fbe29
vinicius.machado <supersoftdev01@outlook.com>
2023-11-28 16:59:44 -0300

    Gerenciar estado

--- lib/one_page.dart
+++ lib/one_page.dart
@@ -1,22 +1,47 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:navegacao_paginas/widgets/custom_button_widget.dart';
 //import 'package:navegacao_paginas/two_page.dart';
 
-class OnePage extends StatelessWidget {
+class OnePage extends StatefulWidget {
   const OnePage({Key? key}) : super(key: key);
 
   @override
+  State<OnePage> createState() => _OnePageState();
+}
+
+class _OnePageState extends State<OnePage> {
+  int valorAleatorio = 0;
+
+  random() async {
+    for (int i = 0; i < 10; i++) {
+      await Future.delayed(Duration(seconds: 1));
+      setState(() {
+        valorAleatorio = Random().nextInt(1000);
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('reconstruindo');
     return Scaffold(
       body: Center(
-        child: CustomButtonWidget(
-          disable: false,
-          onPressed: () {
-            Navigator.of(context)
-                .pushNamed('/twoPage', arguments: 'teste')
-                .then((value) => print(value));
-          },
-          title: 'Ir para Segunda Page',
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Valor é: $valorAleatorio',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 10),
+            CustomButtonWidget(
+              disable: false,
+              onPressed: () => random(),
+              title: 'Ir para Segunda Page',
+            ),
+          ],
         ),
       ),
     );
